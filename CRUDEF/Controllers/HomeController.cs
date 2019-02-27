12f05465e1d39f9cd9af0f673896401d7c9e8af5
manoj kumar <kumar.manoj@thinksys.com>
2019-02-27 16:34:54 +0530

    CRUDEF: some crud operation

---
+++ CRUDEF/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CRUDEF.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CRUDEF.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        private SchoolContext schoolContext;
+        public HomeController(SchoolContext sc)
+        {
+            schoolContext = sc;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Teacher tch)
+        {
+            if (ModelState.IsValid)
+            {
+                schoolContext.Teacher.Add(tch);
+                schoolContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+                return View();
+        }
+    }
+}
---
+++ CRUDEF/Startup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CRUDEF
+{
+    public class Startup
+    {
+      
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
+        {
+            //Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+               .SetBasePath(env.ContentRootPath)
+               .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+               .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+               .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+        public IConfiguration Configuration;
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<SchoolContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+
+            app.UseStaticFiles();
+            app.UseDeveloperExceptionPage();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Create}/{id?}");
+            });
+        }
+    }
+}
